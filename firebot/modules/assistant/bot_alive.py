d70fcbb51b3e4185b3b02d9ff87df59240377644
Iwang-cem <134604500+Iwang-cem@users.noreply.github.com>
2023-05-25 21:20:58 +0700

    Update bot_alive.py

--- firebot/modules/assistant/bot_alive.py
+++ firebot/modules/assistant/bot_alive.py
@@ -2,7 +2,7 @@ from firebot import ALIVE_NAME
 from firebot.modules import currentversion
 
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Unknown"
-PM_IMG = "https://telegra.ph/file/cf228783192e93b60f04a.jpg"
+PM_IMG = "https://telegra.ph//file/b0e85f92ab4f5f60812b2.jpg"
 pm_caption = "➥ **ASSISTANT IS:** `ONLINE`\n\n"
 pm_caption += "➥ **SYSTEMS STATS**\n"
 pm_caption += "➥ **Telethon Version:** `1.23.0` \n"
@@ -11,8 +11,8 @@ pm_caption += "➥ **Database Status:**  `Functional`\n"
 pm_caption += "➥ **Current Branch** : `master`\n"
 pm_caption += f"➥ **Version** : `{currentversion}`\n"
 pm_caption += f"➥ **My Boss** : {DEFAULTUSER} \n\n"
-pm_caption += "➥ **Repo** :  [HERE](https://github.com/TeamEviral/Fire-X)\n"
-pm_caption += "[Support Channel](https://t.me/FIRE_X_CHANNEL)"
+pm_caption += "➥ **Repo** :  [HERE](https://github.com/Iwang-cem/Iwang-bot)\n"
+pm_caption += "[Support Channel](https://t.me/cemarastore99)"
 
 # only Owner Can Use it
 @assistant_cmd("alive", is_args=False)
