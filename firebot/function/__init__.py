becc8a498395b88dce391040231d364440ce5930
Iwang-cem <134604500+Iwang-cem@users.noreply.github.com>
2023-05-25 18:00:10 +0700

    Update __init__.py

--- firebot/function/__init__.py
+++ firebot/function/__init__.py
@@ -500,7 +500,7 @@ async def check_if_subbed(channel_id, event, bot):
 
 async def _ytdl(url, is_it, event, tgbot):
     await event.edit(
-        "`Ok Downloading This Video / Audio - Please Wait.` \n**Powered By Firebot**"
+        "`Ok Downloading This Video / Audio - Please Wait.` \n**Powered By iwangbot**"
     )
     if is_it:
         opts = {
@@ -569,7 +569,7 @@ async def _ytdl(url, is_it, event, tgbot):
         )
         await event.edit(
             file=lol_m,
-            text=f"{ytdl_data['title']} \n**Uploaded Using Fire-X**",
+            text=f"{ytdl_data['title']} \n**Uploaded Using iwang-X**",
         )
         os.remove(f"{ytdl_data['id']}.mp3")
     elif video:
@@ -592,14 +592,14 @@ async def _ytdl(url, is_it, event, tgbot):
             ),
         )
         await event.edit(
-            file=hmmo, text=f"{ytdl_data['title']} \n**Uploaded Using Firebot**"
+            file=hmmo, text=f"{ytdl_data['title']} \n**Uploaded Using iwangbot**"
         )
         os.remove(f"{ytdl_data['id']}.mp4")
 
 
 async def _deezer_dl(word, event, tgbot):
     await event.edit(
-        "`Ok Downloading This Audio - Please Wait.` \n**Powered By firebot**"
+        "`Ok Downloading This Audio - Please Wait.` \n**Powered By iwangbot**"
     )
     link = f"https://api.deezer.com/search?q={word}&limit=1"
     dato = requests.get(url=link).json()
@@ -620,8 +620,8 @@ async def _deezer_dl(word, event, tgbot):
 **Song Name :** {urlhp.get("title")}
 **Duration :** {urlhp.get('duration')} Seconds
 **Artist :** {polu.get("name")}
-Music Downloaded And Uploaded By firebot
-Get Your firebot From Fire-X"""
+Music Downloaded And Uploaded By iwangbot
+Get Your iwangbot From iwang-X"""
     await event.edit("Song Downloaded.  Waiting To Upload. 🥳🤗")
     c_time = time.time()
     uploaded_file = await upload_file(
@@ -635,7 +635,7 @@ Get Your firebot From Fire-X"""
 
     await event.edit(
         file=uploaded_file,
-        text=f"""{urlhp.get("title")} \n**Uploaded Using Firebot**""",
+        text=f"""{urlhp.get("title")} \n**Uploaded Using iwangbot**""",
     )
     os.remove(sname)
 
